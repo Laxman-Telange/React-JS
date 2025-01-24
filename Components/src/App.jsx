3b247d296d5a792a9448867ece478cb01caba8be
Laxman-Telange <laxman.telange25@gmail.com>
2025-01-24 12:28:13 +0530

    Add files via upload

---
+++ Components/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+// import { Car } from "./components/Car";
+// import { SimpleJsx } from "../src/components/SimpleJsx.jsx";
+// import { ListFruits } from "../src/components/ListFruits.jsx";
+import { ConditionalData } from "./components/ConditionalData.jsx";
+
+function App() {
+  return (
+    <>
+      <div className="car">
+        <ConditionalData />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ Components/src/components/ConditionalData.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export class ConditionalData extends React.Component {
+  render() {
+    let greeting = "Hello world";
+    const date = new Date(); //get the current date and time
+    const hours = date.getHours(); //get the current hours
+    const minutes = date.getMinutes(); //get the current minutes
+
+    if (hours < 12) {
+      greeting = "Good Morning";
+    } else if (hours < 18) {
+      greeting = "Good Afternoon";
+    } else {
+      greeting = "Good Evening";
+    }
+
+    return (
+      <div>
+        <h1>{greeting}</h1>
+        <p>
+          The current time is {hours}:{minutes < 10 ? `0${minutes}` : minutes}
+        </p>
+      </div>
+    );
+  }
+}
