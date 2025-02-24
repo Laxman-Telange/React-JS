4ad8ab053e77d477f23489506ab2521843d4e20c
Laxman-Telange <laxman.telange25@gmail.com>
2025-02-24 10:50:14 +0530

    Delete IMDb-Page directory

--- IMDb-Page/src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { Header } from "./Header";
-export { MoviePanel } from "./MoviePannel";
-export { MovieCard } from "./MovieCards";
-export { SearchBar } from "./SearchBar";
-export { Form } from "./MovieForm";
